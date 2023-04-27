271107d1de6058b097c449fdedad139629e74960
asheroto <github@asheroto.com>
2023-04-27 08:32:38 -0500

    Refactored,

--- The Epoch Times - Ad & Paywall Removal.js
+++ The Epoch Times - Ad & Paywall Removal.js
@@ -18,9 +18,33 @@
 /* jshint esversion: 6 */
 
 (function () {
+
+	// Prefix for console log
+	const logPrefix = "[Ads & Paywall Removal]";
+
+	// Console logging function
+	function doLog(msg) {
+		console.log(logPrefix + " " + msg);
+	};
+
+	// Function to wait until an element exists
+	async function waitForElement(selector, callback, checkFrequencyInMs, timeoutInMs) {
+		doLog("Waiting for element: " + selector);
+		let startTimeInMs = Date.now();
+		while (document.querySelector(selector) == null) {
+			await new Promise(resolve => setTimeout(resolve, checkFrequencyInMs));
+			if (timeoutInMs && Date.now() - startTimeInMs > timeoutInMs) {
+				doLog("Completed timeout waiting for element: " + selector);
+				return;
+			}
+		}
+		doLog("Element found: " + selector);
+		callback();
+	}
+
 	// List of CSS changes (instant ad removal)
 	const css = `
-		#ad_right_top_300x250_1 { display: none; }
+		#ad_right_top_300x250_1 { display: none !important; }
 		#article_ad_right_middle_300x250_1,
 		#article_ad_right_middle_300x250_2,
 		#article_ad_right_top_300x250_1,
@@ -36,19 +60,18 @@
 		#in_article_ads_8,
 		#inside_ad_336x280_1,
 		#inside_ad_336x280_2,
-		#landing-page { display: none; }
+		#landing-page { display: none !important; }
 		#main { height: unset !important; overflow: unset !important; }
 		#main > div { border-top: unset; margin-top: 0px; }
 		#modal-COMMON { display: none !important; }
-		#partnership { display: none; }
-		.home-wall { display: none; }
-		.login_wrapper { display: none; }
+		#partnership { display: none !important; }
+		.home-wall { display: none !important; }
+		.login_wrapper { display: none !important; }
 		.right_col.noprint > div { margin: unset !important; }
 		.soft_stikcy { display: none !important; }
-		.top_ad { display: none; }
+		.top_ad { display: none !important; }
 		#footer { display: block !important; }
 	`;
-
 
 	// Apply the styles above to document stylesheet
 	async function applyStyles(css) {
@@ -56,87 +79,73 @@
 		const style = document.createElement("style");
 		head.appendChild(style);
 		style.appendChild(document.createTextNode(css));
+		doLog("Applied styles");
 	}
 
-	// Hide any items that have a prefix listed in the adListIds array
-	async function hideAds() {
-		const adListIds = ["in_article", "inside_ad", "article_ad", "ad_"];
-		const elements = document.querySelectorAll('[id^="' + adListIds.join('"], [id^="') + '"], [class^="' + adListIds.join('"], [class^="') + '"]');
-		elements.forEach(function (element) {
-			element.style.display = "none";
-			console.log("Hidden: " + element);
-		});
-	}
-
-	// Every 0.5 seconds, check if any item from the blacklist array is included in the src value of an item, and if so, hide that item, ends loop after 5 seconds
 	async function removeElements() {
+		// Every 0.5 seconds, check if any item from the blacklist array is included in the src value of an item, and if so, hide that item, ends loop after 5 seconds
 		const blacklist = ["doubleclick.", "amazon-adsystem", "adnxs", "ads."];
 		const tags = ["script", "iframe"];
 
-		// Loop until timeout is reached
-		const startTime = Date.now();
-		while (Date.now() - startTime < 5000) {
-			tags.forEach(function (tag) {
-				const elements = document.querySelectorAll(tag);
-				Array.from(elements).forEach(function (element) {
-					if (blacklist.some(function (url) { return element.src.includes(url); })) {
-						element.style.display = "none";
-						console.log("Hidden: " + element);
-					}
+		let repeat = setInterval(function () {
+			tags.forEach(function (item) {
+				const elements = Array.from(document.getElementsByTagName(item));
+				elements.forEach(function (src) {
+					blacklist.forEach(function (b) {
+						if (src.src.includes(b)) {
+							src.style.setProperty('display', 'none', 'important');
+							console.log(src.src);
+						}
+					});
 				});
 			});
+		}, 500);
 
-			// Wait for 0.5 seconds before checking again
-			await new Promise(resolve => setTimeout(resolve, 500));
-		}
-
-		// Log when removal loop is stopped
-		console.log("Stopped removal loop");
+		// Clear tag removal after 5 seconds
+		setTimeout(function () {
+			clearInterval(repeat);
+			doLog("Stopped removal loop");
+			resolve();
+		}, 5000);
 	}
 
 	// Modal will be hidden in CSS above, but this will help it out a little and enable the scrollbars again
-	waitForElement("#modal-COMMON", () => {
-		const modal = document.querySelector("#modal-COMMON");
-		modal.className = modal.className.replaceAll("is-open", "is-closed");
-		document.body.classList.remove("free_user", "hidden");
-		document.body.style.overflow = "";
-		console.log("Removed modal, enabled scrollbars");
-	}, 250, 9000);
-
-
-	// Function to wait until an element exists
-	async function waitForElement(selector, callback, checkFrequencyInMs, timeoutInMs) {
-		doLog("Waiting for element: " + selector);
-		const startTimeInMs = Date.now();
-		let elapsedTimeInMs = 0;
-		try {
-			while (elapsedTimeInMs < timeoutInMs) {
-				const element = document.querySelector(selector);
-				if (element) {
-					callback(element);
-					doLog("Element found: " + selector);
-					return;
-				} else {
-					await new Promise(resolve => setTimeout(resolve, checkFrequencyInMs));
-					elapsedTimeInMs = Date.now() - startTimeInMs;
-				}
-			}
-			doLog("Timed out waiting for element: " + selector);
-		} catch (error) {
-			doLog("Error waiting for element: " + selector + ". " + error);
-		}
+	async function modalHelper() {
+		waitForElement("#modal-COMMON", function () {
+			const modal = document.querySelector("#modal-COMMON");
+			modal.className = modal.className.replaceAll("is-open", "is-closed");
+			document.body.classList.remove("free_user", "hidden");
+			document.body.style.overflow = "";
+			doLog("Removed modal, enabled scrollbars");
+		}, 250, 5000);
 	}
 
-	// Prefix for console log
-	const logPrefix = "[Ads & Paywall Removal]";
+	// Hide any items that have a prefix listed in the adListIds array
+	async function hideElements() {
+		const adListIds = ["in_article", "inside_ad", "article_ad", "ad_"];
+		let repeat = setInterval(function () {
+			adListIds.forEach(function (adId) {
+				const selector = '[id^="' + adId + '"], [class^="' + adId + '"]';
+				const isId = selector.startsWith('[id=');
+				const elements = Array.from(document.querySelectorAll(selector));
+				elements.forEach(function (element) {
+					element.style.setProperty('display', 'none', 'important');
+					doLog("Hidden: " + (isId ? "ID " : "Class ") + adId);
+				});
+			});
+		}, 500);
 
-	// Console logging function
-	function doLog(msg) {
-		console.log(logPrefix + " " + msg);
-	};
+		// Clear tag removal after 5 seconds
+		setTimeout(function () {
+			clearInterval(repeat);
+			doLog("Stopped removal loop");
+			resolve();
+		}, 5000);
+	}
 
-	// Run everything
 	applyStyles(css);
-	hideAds();
 	removeElements();
-});
+	modalHelper();
+	hideElements();
+
+})();
